e3847457ccfd608ebc002f66a19fba391d45d68c
DAM <92580076+Ivan-APW@users.noreply.github.com>
2022-06-30 20:12:29 +0200

    Add files via upload

--- sfp_InfoIP.py
+++ sfp_InfoIP.py
@@ -15,6 +15,7 @@ import re
 from spiderfoot import SpiderFootEvent, SpiderFootPlugin
 import socket
 import portscanner
+import platform
 
 
 class sfp_new_module(SpiderFootPlugin):
@@ -76,6 +77,15 @@ class sfp_new_module(SpiderFootPlugin):
             print("IP : ", host_ip)
         except:
             print("Unable to get Hostname and IP")
+        
+        #Extaer informacion basica del equipo
+        my_system = platform.uname()
+        print(f"System: {my_system.system}")
+        print(f"Node Name: {my_system.node}")
+        print(f"Release: {my_system.release}")
+        print(f"Version: {my_system.version}")
+        print(f"Machine: {my_system.machine}")
+        print(f"Processor: {my_system.processor}")
 
         evt = SpiderFootEvent(self, event)
         self.notifyListeners(evt)
